55230335c6f2756ffe5aa53dc73e167e18caa9b0
gbeglaryan <gbeglaryan@cloudlinux.com>
2026-09-19 16:22:47 +0400

    Loop, queue led on

--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,14 @@
 #include <BitUtils.h>
 
 void setup() {
-    // Pin 9 = PB1 -> делаем его выходом (кран переведён в режим "сам качает воду")
-    DDRB |= (1 << DDB1);
-
     // Pin 3 = PD3 (PORTD) -> выход
     DDRD |= (1 << DDD3);
 }
 
 void loop() {
-    // PORTB |= (1 << PB1);
-    setBit(PORTB, PB1);
-    // PORTD &= ~(1 << PD3);
-    // PORTD &= ~(1 << PD7);
-    writeBits(PORTD,3,5,0b10000);
-    delay(500);
-
-    // PORTB &= ~(1 << PB1);
-    // PORTD |= (1 << PD3);
-    // PORTD &= ~(1 << PD7);
-    // delay(500);
-
-    // PORTB &= ~(1 << PB1);
-    clearBit(PORTB, PB1);
-    // PORTD &= ~(1 << PD3);
-    // PORTD |= (1 << PD7);
-    writeBits(PORTD,3,5,0b00001);
-    delay(500);
+    for (int i = 0; i <= 4; i += 2) {
+        uint8_t mask = 1 << i;
+        writeBits(PORTD, 3, 5, mask);
+        delay(500);
+    }
 }
