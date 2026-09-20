77c254222c6b6ed04ea310118d697ba0944d9cdc
gbeglaryan <gbeglaryan@cloudlinux.com>
2026-09-21 02:24:11 +0400

    LED on/off with button. DeBounce handled

--- src/main.cpp
+++ src/main.cpp
@@ -2,35 +2,39 @@
 #include <avr/io.h>
 
 constexpr uint8_t LED_PIN = 1;
-constexpr uint8_t BUTTON_PIN = 2; 
+constexpr uint8_t BUTTON_PIN = 2;
+constexpr unsigned long DEBOUNCE_MS = 30;
 
-bool ledState = false;
-// bool lastButtonState = HIGH;
-bool lastButtonState = true;
+bool rawState = true;
+bool lastRawState = true;
+bool debouncedState = true;
+bool lastDebouncedState = true;
+
+unsigned long lastChangeTime = 0;
 
 void setup() {
-    // pinMode(LED_PIN, OUTPUT);
     DDRB |= (1 << LED_PIN);
     PORTB &= ~(1 << LED_PIN);
 
-    // pinMode(BUTTON_PIN, INPUT_PULLUP);
     DDRD &= ~(1 << BUTTON_PIN);
     PORTD |= (1 << BUTTON_PIN);
 }
- 
-void loop() {
-    // const bool buttonState = digitalRead(BUTTON_PIN);
-    const bool buttonState = (PIND & (1 << BUTTON_PIN)) != 0;
 
-    // if(lastButtonState == HIGH && buttonState == LOW){
-    if(lastButtonState && !buttonState){
-        delay(100); // temporary, to avoid button bounce
-        
-        ledState = !ledState;
-        // digitalWrite(LED_PIN, ledState);
-        PORTB ^= (1 << LED_PIN);
+void loop() {
+    rawState = (PIND & (1 << BUTTON_PIN)) != 0;
+
+    if (rawState != lastRawState) {
+        lastChangeTime = millis();
     }
 
-    lastButtonState = buttonState;
+    if ((millis() - lastChangeTime) > DEBOUNCE_MS && rawState != debouncedState) {
+        debouncedState = rawState;
+
+        if (lastDebouncedState && !debouncedState) {
+            PORTB ^= (1 << LED_PIN); // toggle
+        }
+        lastDebouncedState = debouncedState;
+    }
+
+    lastRawState = rawState;
 }
- 
