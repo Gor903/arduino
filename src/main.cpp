9390250ed7c83a43863b058ff9be39427f3f922d
gbeglaryan <gbeglaryan@cloudlinux.com>
2026-09-22 01:24:16 +0400

    LED on/off with percentage from potentiometer. LEDs can be any BIT connected

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,32 @@
 #include <Arduino.h>
 
 constexpr uint8_t POT_PIN = A0;
+
 constexpr uint8_t GREEN = 1;
 constexpr uint8_t YELLOW = 2;
 constexpr uint8_t ORANGE = 3;
 constexpr uint8_t RED = 4;
 
+constexpr uint8_t LEDS[] = {
+    GREEN,
+    YELLOW,
+    ORANGE,
+    RED
+};
+
 void setup() {
-    pinMode(GREEN, OUTPUT);
-    pinMode(YELLOW, OUTPUT);
-    pinMode(ORANGE, OUTPUT);
-    pinMode(RED, OUTPUT);
+    for(size_t i = 0; i < sizeof(LEDS); i++){
+        pinMode(LEDS[i], OUTPUT);
+    }
 }
 
 void loop() {
-    int potValue = analogRead(POT_PIN);
-    int percent = (long)potValue * 100 / 1023;
+    unsigned int potValue = analogRead(POT_PIN);
+    unsigned int percent = potValue * 100L / 1023;
 
-    digitalWrite(GREEN, percent > 0);
-    digitalWrite(YELLOW, percent > 25);
-    digitalWrite(ORANGE, percent > 50);
-    digitalWrite(RED, percent > 75);
+    for(size_t i = 0; i < sizeof(LEDS); i++){
+        digitalWrite(LEDS[i], percent <= 25 * (i+1) && percent > i * 25);
+    }
 
     delay(10);
 }
